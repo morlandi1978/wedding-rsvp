a774ff378b4a9241c01a2e5549dc7f27f576da90
Maurizio Orlandi <maurizio.orlandi@ictlabs.it>
2026-03-31 09:19:06 +0200

    cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,7 +28,7 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.FRONTEND_URL, "http://localhost:3000", "http://localhost:8080"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
